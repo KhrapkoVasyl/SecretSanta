1884807bd18ea57f1a6d56267b43e6265cc52a15
VasylKhrapko <khrapko2002@gmail.com>
2022-01-17 22:30:56 +0200

    Added new options into registration validation

--- src/config.js
+++ src/config.js
@@ -3,9 +3,11 @@
 const PREFIX = '/api/v1';
 const MIN_PLAYERS_NUMBER = 3;
 const MAX_PLAYERS_NUMBER = 500;
+const MAX_WISHES_NUMBER = 10;
 
 module.exports = {
   PREFIX,
   MIN_PLAYERS_NUMBER,
   MAX_PLAYERS_NUMBER,
+  MAX_WISHES_NUMBER,
 };
--- src/utils/signUpValidationScheme.js
+++ src/utils/signUpValidationScheme.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { check, body } = require('express-validator');
+const { MAX_WISHES_NUMBER } = require('../config');
 
 const signUpValidationScheme = [
   body('name').trim().not().isEmpty().withMessage('Name must be specified'),
@@ -9,12 +10,27 @@ const signUpValidationScheme = [
     .not()
     .isEmpty()
     .withMessage('Surname must be specified'),
-  body('wishes').not().isEmpty().withMessage('Wishlist must be completed'),
-  check('wishes.*')
+  body('wishes')
+    .not()
+    .isEmpty()
+    .withMessage('Wishlist must be completed')
+    .custom(wishArr => {
+      if (wishArr.length > MAX_WISHES_NUMBER) {
+        console.log('hello');
+        throw new Error(
+          // eslint-disable-next-line max-len
+          `The number of given wishes is greater than the allowed maximum (${MAX_WISHES_NUMBER})`
+        );
+      }
+      return true;
+    }),
+  body('wishes.*')
     .trim()
     .not()
     .isEmpty()
-    .withMessage('Wishname must be specified'),
+    .withMessage('Wish name must be specified')
+    .isLength({ min: 3 })
+    .withMessage('Wish name must contain at least 3 letters'),
 ];
 
 module.exports = signUpValidationScheme;
